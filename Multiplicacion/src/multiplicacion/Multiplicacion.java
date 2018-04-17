49ce41d844994e4b879e18b04269847febde4f96
Chalo Salvador <chalosalvador@gmail.com>
2018-04-17 17:25:29 -0500

    Add files via upload

---
+++ Multiplicacion/src/multiplicacion/Multiplicacion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multiplicacion;
+
+/**
+ *
+ * @author chalosalvador
+ */
+public class Multiplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("2 x 1 = 2");
+        System.out.println("2 x 2 = 4");
+        System.out.println("2 x 3 = 6");
+        System.out.println("2 x 4 = 8");
+        System.out.println("2 x 5 = 10");
+        System.out.println("2 x 6 = 12");
+    }
+    
+}
